602732d186398625f28170c28f2088ce4f0f2c22
C. Alan Zoppa <alan.zoppa@gmail.com>
2016-11-11 19:14:20 -0600

    All the basics work

--- lib/va_dataset_generator.rb
+++ lib/va_dataset_generator.rb
@@ -6,19 +6,14 @@ require 'pry'
 
 class VaDatasetGenerator
   attr_accessor :uri, :agent, :detail_ids, :data
-  def self.stuff
-    puts 'stuff'
-  end
-
 
   PATTERN = /javascript:void\(setSingleValue\('(.*)'\)\);/
-  #URI.encode_www_form("q" => "ruby", "lang" => "en")
 
-
-  def initialize
+  def initialize(limit=nil)
     @uri = "https://vip.vba.va.gov/portal/VBAH/VBAHome/condopudsearch?paf_portalId=default&paf_communityId=100002&paf_pageId=500002&paf_dm=full&paf_gear_id=800001&paf_gm=content&paf_ps=_rp_800001_condoName%3D1_%26_rp_800001_approvedOnly%3D1_yes%26_rp_800001_condoId%3D1_%26_ps_800001%3Dmaximized%26_pid%3D800001%26_rp_800001_county%3D1_%26_rp_800001_stateCode%3D1_IL%26_md_800001%3Dview%26_rp_800001_cpbaction%3D1_performSearchPud%26_st_800001%3Dmaximized%26_rp_800001_reportType%3D1_summary%26_rp_800001_regionalOffice%3D1_%26_rp_800001_city%3D1_CHICAGO&_requestid=811905"
     @agent = Mechanize.new
     @page = @agent.get(@uri)
+    @limit = limit
 
     @refids = []
   
@@ -33,21 +28,23 @@ class VaDatasetGenerator
 
 
   def gather_ids
-    @detail_ids = []
-    @page.links.each_with_index do |link, index|
-      break if index > 50
-      if is_detail_page?(link)
-        detail_ids << detail_value(link.attributes["href"])
+    unless defined? @detail_ids
+      @detail_ids = []
+      @page.links.each_with_index do |link, index|
+        break if !@limit.nil? && index > @limit
+        if VaDatasetGenerator.is_detail_page?(link)
+          detail_ids << VaDatasetGenerator.detail_value(link.attributes["href"])
+        end
       end
     end
     @detail_ids
   end
 
-  def is_detail_page?(link)
+  def self.is_detail_page?(link)
     link.attributes.keys.include?("href") && link.attributes["href"].include?("setSingleValue")
   end
 
-  def detail_value(str)
+  def self.detail_value(str)
     str.gsub(PATTERN, '\1')
   end
 
@@ -57,22 +54,15 @@ class VaDatasetGenerator
   end
 
   def get_detail(id)
-    puts 'fetching page'
+     #puts "detail for #{id}"
+    # honestly what is this fucked http param with a bunch of other params embedded in it
     uri = "https://vip.vba.va.gov/portal/VBAH/VBAHome/condopudsearch?paf_portalId=default&paf_communityId=100002&paf_pageId=500002&paf_dm=full&paf_gear_id=800001&paf_gm=content&paf_ps=_pm_800001%3Dview%26_md_800001%3Dview%26_rp_800001_cpbaction%3D1_viewPudDetails%26_ps_800001%3Dmaximized%26_st_800001%3Dmaximized%26_pid%3D800001%26_rp_800001_sortLetter%3D1_%26_rp_800001_singledetail%3D1_#{id}&_requestid=816752"
-    page = @agent.get(uri)
-    out = {"id" => id}
 
-    pairs = page.search('table.inputpanelfields tr').map do |tr|
-      td_nodes = tr.children.map {|n| n.text.gsub(/\t|\n/, '').strip}
-      td_nodes.reject! {|t| t == ""}
-      td_nodes
+    pairs = @agent.get(uri).search('table.inputpanelfields tr').map do |tr|
+      tr.children.map {|n| n.text.gsub(/\t|\n/, '').strip}.reject {|t| t == ""}
     end
 
-    pairs.each do |k,v|
-      out[k] = v
-    end
-
-    return out
+    pairs.to_h.merge({"id" => id})
   end
 
 
--- spec/va_dataset_generator_spec.rb
+++ spec/va_dataset_generator_spec.rb
@@ -1,17 +1,62 @@
 require "spec_helper"
 require 'pry'
+require 'vcr'
+require 'ostruct'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
+
+
 
 describe VaDatasetGenerator do
   it "has a version number" do
     expect(VaDatasetGenerator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    va = VaDatasetGenerator.new
-    va.gather_ids
-    va.gather_data
-    binding.pry
-    #id = va.detail_ids[0]
-    #va.get_detail(va.detail_page_uri(id))
+  describe "functional methods" do
+    before(:all) {
+      @link = OpenStruct.new
+      @link.attributes = {'href' => "javascript:void(setSingleValue('559791'));"}
+    }
+
+    it "can test strings for detail data" do
+      expect(VaDatasetGenerator.is_detail_page?(@link)).to eql true
+    end
+
+    it "can extract the detail id" do
+      expect(VaDatasetGenerator.detail_value(@link.attributes['href'])).to eql '559791'
+    end
+
+  end
+
+  context "Fetching VA details", :vcr do
+    before(:each) do
+      @va = VaDatasetGenerator.new(150)
+    end
+    #it "should find headers" do
+      #allow(@va).to receive(:gather_ids)
+      #foo = @va.gather_ids
+      #expect(@va).to have_received(:gather_ids)
+      #binding.pry
+    #end
+    it "should be able to fetch a single detail" do
+      actual = @va.get_detail('529550')
+      reference = {
+          "id"=>"529550",
+          "Condo Name (ID)"=>"1000 WEST WASHINGTON LOFTS (002357)",
+          "Address"=>"1000 W WASHINGTON BLVDCHICAGO IL 60607 COOK ",
+          "Status"=>"Accepted Without Conditions",
+          "Last Update"=>"Unavailable",
+          "Request Received Date"=>"01/08/2015",
+          "Review Completion Date"=>"01/26/2015"
+        }
+      reference.keys.each do |key|
+        expect(actual[key]).to eql reference[key]
+      end
+      expect(actual.keys.sort).to eql reference.keys.sort
+    end
   end
 end
